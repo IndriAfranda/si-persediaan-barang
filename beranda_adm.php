cc25929c11f838d80ef1e9398d71381cb34a1777
IndriAfranda <72698803+IndriAfranda@users.noreply.github.com>
2021-01-18 23:50:03 +0700

    Add files via upload

---
+++ beranda_adm.php
@@ -0,0 +1,36 @@
+<div class="container">
+    <div class="row">
+        <div class ="col-xs-12">
+
+            
+        </div>
+    </div>
+    <div class="row">
+        <!--colomn kedua-->
+        <div class="col-sm-12 col-xs-12">
+            <div class="panel panel-info">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Halaman Administrator SI Persediaan Barang Gudang</h3>
+                </div>
+                <div class="panel-body">
+                     <table id="dtskripsi" class="table table-bordered table-striped table-hover">
+                       <thead>
+                          <p align="center"><img src="img/sip.jpg"></img></p>
+                          <p class="label-title" align="center"><strong>Sistem Informasi Persediaan Barang Gudang</strong></p>
+                          <p class="col-sm-12 col-xs-12" align="center">
+                            Selamat datang di halaman administrator Sistem Informasi Persediaan Barang Gudang.<br>
+                            Melalui halaman ini dapat dilakukan pengelolaan data barang, jenis barang. <br>
+                            Serta bisa melihat transaksi barang masuk dan barang keluar<br>
+                            dan juga pengelolaan laporan stok barang masuk dan keluar <br>
+                            Akses menu User untuk mengelola informasi tentang user yang login.<br><br><br>
+
+                          </p>
+
+                         </thead>
+
+
+                    </table>
+                </div>
+            </div>
+        </div>
+</div>
---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            Copyright &COPY; <?= date('Y')?> Open Source 3 |
+            <a href="#">Oleh</a>
+            <a href="#">Indri, Eka, Sonia</a>
+        </div>
+    </div>
+</div>
